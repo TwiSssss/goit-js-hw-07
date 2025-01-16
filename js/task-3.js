b691bdc34b05ef507c2433129ea68d9ee7d5d370
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-01-16 11:34:28 +0200

    commit message

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,11 @@
-class StringBuilder {
-    #value;
-    constructor(initialValue) {
-        this.#value = initialValue;
-    }
-    getValue() {
-        return this.#value;
-    }
-    padEnd(str) {
-        this.#value += str;
-    }
-    padStart(str) {
-        this.#value = str + this.#value;
-    }
-    padBoth(str) {
-        this.padEnd(str);
-        this.padStart(str);
-    }
-}
+const textInput = document.querySelector("input#name-input");
+const output = document.querySelector("span#name-output");
 
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+textInput.addEventListener("input", (event) => {
+    const username = event.currentTarget.value.trim();
+    if (username) {
+        output.textContent = username;
+      } else {
+        output.textContent = "Anonymous";
+      }
+});
---
+++ js/task-6.js
@@ -0,0 +1,35 @@
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const inputFild = document.querySelector("#controls input");
+const boxesCont = document.querySelector("#boxes");
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+    const amount = inputFild.value;
+    if (amount < 1 || amount > 100) {
+        inputFild.value = "";
+        return;
+    }
+    boxesCont.innerHTML = "";
+
+    for (let i = 0; i < amount; i++) {
+        const divElem = document.createElement("div");
+        divElem.style.width = `${30 + i * 10}px`;
+        divElem.style.height = `${30 + i * 10}px`;
+        divElem.style.backgroundColor = getRandomHexColor();
+        boxesCont.appendChild(divElem);
+    }
+    inputFild.value = "";
+}
+
+function destroyBoxes() {
+    boxesCont.innerHTML = "";
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
